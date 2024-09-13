ee0901a41fb80d030ef35a7e276c4b8f28856ce7
Ralph Meertens <ralph@zilliqa.com>
2024-09-13 19:14:41 +0200

    new server json and minting backend

--- dest/index.js
+++ dest/index.js
@@ -22,45 +22,133 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const sdk_1 = require("@imtbl/sdk");
 const fs = __importStar(require("fs"));
 const cors_1 = __importDefault(require("cors"));
 const http_1 = __importDefault(require("http"));
-const foxImageUrl = 'https://rose-ministerial-termite-701.mypinata.cloud/ipfs/Qmd3oT99HypRHaPfiY6JWokxADR5TzR1stgonFy1rMZAUy';
-const skinImageUrl = 'https://rose-ministerial-termite-701.mypinata.cloud/ipfs/QmNZeG8wkW3mFw4PrqEj34NPA88impcvemYjhAkJAM4YcK';
-var bodyParser = require('body-parser');
+const dotenv_1 = __importDefault(require("dotenv"));
+const ethers_1 = require("ethers");
+dotenv_1.default.config(); //env file
 const app = (0, express_1.default)();
+//app.use(morgan('dev')); // Logging
 app.use((0, cors_1.default)());
-app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
+app.use(express_1.default.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(express_1.default.json()); // parse application/json
 const router = express_1.default.Router();
-router.post("/createmetadata/:id", async (req, res) => {
-    let nftID = req.params.id;
-    const json = {
-        id: nftID,
-        name: req.body.name,
-        image: req.body.image,
-        attributes: req.body.attributes
-    };
-    fs.writeFile('metadata/' + nftID + '.json', JSON.stringify(json), function (err) {
+const privateKey = process.env.PRIVATE_KEY;
+const zkEvmProvider = new ethers_1.providers.JsonRpcBatchProvider('https://rpc.testnet.immutable.com');
+const gasOverrides = {
+    // Use parameter to set tip for EIP1559 transaction (gas fee)
+    maxPriorityFeePerGas: 10e9, // 10 Gwei. This must exceed minimum gas fee expectation from the chain
+    maxFeePerGas: 15e9, // 15 Gwei
+};
+//router
+router.post('/mint/NFT', (req, res) => {
+    console.log("asd");
+    MintItem(req.body.to, req.body.name, req.body.image, req.body.attributes);
+});
+router.get('/item/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    //need to get the file
+    fs.readFile('metadata/' + req.params.id + ".json", 'utf8', function (err, data) {
+        if (err)
+            return;
+        res.writeHead(200);
+        res.end(data);
+    });
+}));
+app.use('/', router);
+http_1.default.createServer(app).listen(3000, () => console.log('Listening on port 3000'));
+class NftMetadata {
+    constructor(p_id, p_name, p_image, p_attributes) {
+        this.id = p_id;
+        this.name = p_name;
+        this.image = p_image;
+        this.attributes = p_attributes;
+    }
+}
+function MintItem(to, p_name, p_imageUri, p_attributes) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { Environment } = sdk_1.config;
+        const { IMXClient, imxClientConfig } = sdk_1.x;
+        const environment = Environment.SANDBOX;
+        const imxClient = new IMXClient(imxClientConfig({ environment }));
+        let nextTokenID = yield NextTokenId("0xe690da5e67df083fe198d2af0d17aad420ac1973", imxClient);
+        console.log("nextTokenID" + nextTokenID);
+        let metadata = new NftMetadata(nextTokenID, p_name, p_imageUri, p_attributes);
+        CreateMetadata(metadata);
+        try {
+            if (privateKey) {
+                // Get the address to mint to
+                //let to: string = req.body.to ?? null;
+                // Get the quantity to mint if specified, default is one
+                // Connect to wallet with minter role
+                const signer = new ethers_1.Wallet(privateKey).connect(zkEvmProvider);
+                // Specify the function to call
+                const abi = ['function mintByQuantity(address to, uint256 quantity)'];
+                // Connect contract to the signer
+                const contract = new ethers_1.Contract("0xe690da5e67df083fe198d2af0d17aad420ac1973", abi, signer);
+                // Mints the number of tokens specified
+                const tx = yield contract.mintByQuantity(to, 1, gasOverrides);
+                yield tx.wait();
+                console.log("succes");
+                //return res.status(200).json({});
+            }
+            else {
+                console.log("failed");
+                //return res.status(500).json({});
+            }
+        }
+        catch (error) {
+            //check if we have the metadata created ifso remove it....
+            console.log(error);
+            //return res.status(400).json({ message: 'Failed to mint to user' });
+        }
+    });
+}
+function CreateMetadata(metaData) {
+    console.log("CreateMetadata");
+    fs.writeFile('metadata/' + metaData.id + '.json', JSON.stringify(metaData), function (err) {
         if (err) {
             return console.error(err);
         }
         console.log("File created!");
     });
-});
-router.get('/item/:id', async (req, res) => {
-    //need to get the file
-    await fs.readFile('metadata/' + req.params.id + ".json", 'utf8', function (err, data) {
-        if (err)
-            return;
-        res.writeHead(200);
-        res.end(JSON.stringify(data));
+}
+function NextTokenId(collectionAddress, imxClient) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let remaining = 0;
+        let cursor;
+        let tokenId = 0;
+        do {
+            // eslint-disable-next-line no-await-in-loop
+            const assets = yield imxClient.listAssets({
+                collection: collectionAddress,
+                cursor,
+            });
+            remaining = assets.remaining;
+            cursor = assets.cursor;
+            for (const asset of assets.result) {
+                const id = parseInt(asset.token_id, 10);
+                if (id > tokenId) {
+                    tokenId = id;
+                }
+            }
+        } while (remaining > 0);
+        return tokenId + 1;
     });
-});
-app.use('/', router);
-http_1.default.createServer(app).listen(3000, () => console.log('Listening on port 3000'));
+}
+;
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,38 @@
 import express, {
-  Express,
-  Router,
-  Request,
-  Response,
-} from 'express';
-
+    Express,
+    Router,
+    Request,
+    Response,
+  } from 'express';
+import { config, x } from '@imtbl/sdk';
 import * as fs from 'fs';
 import cors from 'cors';
 import http from 'http';
+import dotenv from 'dotenv';
+import {providers, Wallet, Contract } from 'ethers';
+dotenv.config(); //env file
 
-
-const foxImageUrl = 'https://rose-ministerial-termite-701.mypinata.cloud/ipfs/Qmd3oT99HypRHaPfiY6JWokxADR5TzR1stgonFy1rMZAUy';
-const skinImageUrl = 'https://rose-ministerial-termite-701.mypinata.cloud/ipfs/QmNZeG8wkW3mFw4PrqEj34NPA88impcvemYjhAkJAM4YcK';
-
-var bodyParser = require('body-parser')
 const app: Express = express();
-
-
+//app.use(morgan('dev')); // Logging
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))// parse application/x-www-form-urlencoded
-app.use(bodyParser.json())// parse application/json
+app.use(express.urlencoded({ extended: false }))// parse application/x-www-form-urlencoded
+app.use(express.json())// parse application/json
 const router: Router = express.Router();
 
+const privateKey = process.env.PRIVATE_KEY;
+const zkEvmProvider = new providers.JsonRpcBatchProvider('https://rpc.testnet.immutable.com');
 
-
-router.post("/createmetadata/:id",
-  async (req: Request, res: Response) => {
-    let nftID = req.params.id
-    const json = {
-      id: nftID,
-      name: req.body.name,
-      image: req.body.image,
-      attributes: req.body.attributes
-    };
-
-    fs.writeFile('metadata/' + nftID + '.json', JSON.stringify(json),  function(err) {
-      if (err) {
-          return console.error(err);
-      }
-      console.log("File created!");
-  });
-});
+const gasOverrides = {
+  // Use parameter to set tip for EIP1559 transaction (gas fee)
+  maxPriorityFeePerGas: 10e9, // 10 Gwei. This must exceed minimum gas fee expectation from the chain
+  maxFeePerGas: 15e9, // 15 Gwei
+};
+//router
+router.post('/mint/NFT', (req: Request, res: Response) => {
+  console.log("asd");
+    MintItem(req.body.to, req.body.name, req.body.image, req.body.attributes);
+  },
+);
 
 
 router.get( 
@@ -54,10 +46,108 @@ router.get(
     });
   });
 
+  app.use('/', router);
 
-app.use('/', router);
+  http.createServer(app).listen(
+    3000,
+    () => console.log('Listening on port 3000'),
+  );
 
-http.createServer(app).listen(
-  3000,
-  () => console.log('Listening on port 3000'),
-);
+
+class NftMetadata
+  { 
+    id: number;
+    name: string;
+    image: string;
+    attributes: string;
+    public constructor(p_id: number, p_name: string, p_image:string, p_attributes:string)
+    {
+      this.id = p_id;
+      this.name = p_name + " " + p_id;
+      this.image = p_image;
+      this.attributes = p_attributes;
+    }
+  }
+
+async function MintItem(to:string, p_name: string, p_imageUri: string, p_attributes: string)//, p_collectionAddress: string
+{  
+  const { Environment } = config;
+  const { IMXClient, imxClientConfig } = x;
+  const environment = Environment.SANDBOX;
+  const imxClient = new IMXClient(imxClientConfig({ environment }));
+  let nextTokenID = await NextTokenId("0xe690da5e67df083fe198d2af0d17aad420ac1973", imxClient);
+  console.log("nextTokenID" + nextTokenID);
+  let metadata = new NftMetadata(nextTokenID, p_name, p_imageUri, p_attributes);
+  CreateMetadata(metadata);
+  
+  try {
+    
+    if (privateKey) {
+      // Get the address to mint to
+      //let to: string = req.body.to ?? null;
+      // Get the quantity to mint if specified, default is one
+      
+
+      // Connect to wallet with minter role
+      const signer = new Wallet(privateKey).connect(zkEvmProvider);
+
+      // Specify the function to call
+      const abi = ['function safeMint(address to, uint256 nextTokenID)'];
+      // Connect contract to the signer
+      const contract = new Contract("0x187cd0e729cfb925fed47c7ad0c10ca2f4d7d1c1", abi, signer);
+
+      // Mints the number of tokens specified
+      const tx = await contract.mintByQuantity(to, 1, gasOverrides);
+      await tx.wait();
+      console.log("succes");
+      //return res.status(200).json({});
+    } else {
+      console.log("failed");
+      //return res.status(500).json({});
+    }
+
+  } catch (error) {
+    //check if we have the metadata created ifso remove it....
+    console.log(error);
+    //return res.status(400).json({ message: 'Failed to mint to user' });
+  }
+}
+
+
+function CreateMetadata(metaData: NftMetadata)
+{
+  console.log("CreateMetadata");
+  fs.writeFile('metadata/' + metaData.id + '.json', JSON.stringify(metaData),  function(err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("File created!");
+  });
+}
+
+async function NextTokenId(collectionAddress: string, imxClient: any) 
+{
+
+  let remaining = 0;
+  let cursor: string | undefined;
+  let tokenId = 0;
+
+  do {
+    // eslint-disable-next-line no-await-in-loop
+    const assets = await imxClient.listAssets({
+      collection: collectionAddress,
+      cursor,
+    });
+    remaining = assets.remaining;
+    cursor = assets.cursor;
+
+    for (const asset of assets.result) {
+      const id = parseInt(asset.token_id, 10);
+      if (id > tokenId) {
+        tokenId = id;
+      }
+    }
+  } while (remaining > 0);
+
+  return tokenId + 1;
+};
